615a15a9bb27500bf6dda2d766bfd4d4ac5e029a
Oto Havle <havleoto@gmail.com>
2013-10-05 09:24:29 +0200

    Improve error reporting in interpreted built-in combiners. Add ground binding: make-error-object

--- src/modules/errors.asm
+++ src/modules/errors.asm
@@ -19,6 +19,35 @@ app_error:
     mov ebx, cdr(ebx)
     jmp .throw
 
+app_make_error_object:
+  .A1:
+    mov ecx, nil_tag
+  .A2:
+    mov edx, inert_tag
+  .A3:
+    cmp bl, string_tag
+    jne .invalid_argument
+    cmp dl, symbol_tag
+    je .ok
+    cmp dl, inert_tag
+    je .ok
+    mov ebx, edx
+  .invalid_argument:
+    mov eax, err_invalid_argument
+    mov ecx, symbol_value(rom_string_make_error_object)
+    jmp rn_error
+  .ok:
+    mov esi, ecx
+    mov ecx, (error_header >> 8)
+    call rn_allocate
+    mov [eax + error.header], dword error_header
+    mov [eax + error.message], ebx
+    mov [eax + error.irritants], esi
+    mov [eax + error.source], edx
+    mov ecx, error_continuation
+    mov [eax + error.cc], ecx
+    jmp [ebp + cont.program]
+
 app_error_object_message:
   .A1:
     mov esi, symbol_value(rom_string_error_object_message)
